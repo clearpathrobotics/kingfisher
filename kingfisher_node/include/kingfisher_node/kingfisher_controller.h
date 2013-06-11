3dec51b4ceb192b5742434062a6340a17c64e08f
Prasenjit Mukherjee <pmukherj@clearpathrobotics.com>
2013-06-10 20:40:58 -0400

    thrust management added

--- kingfisher_node/include/kingfisher_node/kingfisher_controller.h
+++ kingfisher_node/include/kingfisher_node/kingfisher_controller.h
@@ -27,19 +27,19 @@ class ForceCompensator
     private:
         ros::NodeHandle node_;
         ros::Publisher cmd_pub;
+        ros::Publisher eff_pub;
 
     public:
 
         ForceCompensator(ros::NodeHandle &n): node_(n) {
             cmd_pub = node_.advertise<kingfisher_msgs::Drive>("cmd_drive",1000);
+            eff_pub = node_.advertise<geometry_msgs::Wrench>("eff_wrench",1000);
         }
         ~ForceCompensator() {
         }
 
         double get_output (double thrust) {
             //saturate
-            thrust = std::min(MAX_FWD_THRUST,thrust);
-            thrust = std::max(-1*MAX_BCK_THRUST,thrust);
 
             double output = 0;
             if (thrust > 0)
@@ -49,21 +49,71 @@ class ForceCompensator
             return output;    
         }
 
+        double saturate_thrusters (double thrust) {
+            thrust = std::min(MAX_FWD_THRUST,thrust);
+            thrust = std::max(-1*MAX_BCK_THRUST,thrust);
+            return thrust;
+        }
+
 
         void update_forces (geometry_msgs::Wrench output) {
             kingfisher_msgs::Drive cmd_output;
-            double forward = output.force.x;
-            double yaw = output.force.z;
+            double fx = output.force.x;
+            double tauz = output.force.z;
 
-            double left_thrust = forward/2.0;
-            double right_thrust = forward/2.0;
+
+            //yaw torque maxed out at max torque achievable with the help of reverse thrust
+            double max_tauz = MAX_BCK_THRUST*2*BOAT_WIDTH;
+            tauz = std::min(tauz, max_tauz);
+            tauz = std::max(tauz, -max_tauz);
+
+            //Guarantee atleast max yaw torque
+            double left_thrust = tauz/(2*BOAT_WIDTH); 
+            double right_thrust = -tauz/(2*BOAT_WIDTH);
+
+            //Provide maximum allowable thrust after yaw torque is guaranteed 
+            double max_fx = 0;
+            if (tauz > 0) {
+                if (fx > 0) { //forward thrust on the left thruster will be limiting factor 
+                    max_fx = (MAX_FWD_THRUST - left_thrust) * 2;
+                    fx = std::min(max_fx,fx);
+                }
+                else { //backward thrust on the right thruster will be limiting factor
+                    max_fx = (MAX_BCK_THRUST - right_thrust) * 2;
+                    fx = std::max(max_fx,fx);
+                }
+            }
+            else { 
+                if (fx > 0 ) {
+                    max_fx = (MAX_FWD_THRUST - right_thrust) * 2;
+                    fx = std::min(max_fx,fx);
+                }
+                else {
+                    max_fx = (MAX_BCK_THRUST - left_thrust) * 2;
+                    fx = std::max(max_fx,fx);
+                }
+            }
+
+            left_thrust += fx /2.0;
+            right_thrust += fx/2.0;
             
-            left_thrust-=yaw/(2*BOAT_WIDTH); 
-            right_thrust += yaw/(2*BOAT_WIDTH); 
+            left_thrust = saturate_thrusters (left_thrust);
+            right_thrust = saturate_thrusters (right_thrust);
 
             cmd_output.left = get_output (left_thrust);
             cmd_output.right = get_output (right_thrust);
+            cmd_pub.publish(cmd_output);
+
+            pub_effective_wrench(left_thrust, right_thrust);
         }
+
+        void pub_effective_wrench(double left_thrust,double right_thrust) {
+            geometry_msgs::Wrench effective_output;
+            effective_output.force.x = left_thrust + right_thrust;
+            effective_output.torque.z = (left_thrust - right_thrust)/BOAT_WIDTH;
+            eff_pub.publish(effective_output);  
+        }
+
 };
 
 
